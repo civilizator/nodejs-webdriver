cb6122d0e6f5760585de347415384747d55f1579
civilizator <stan@argaatas.com>
2019-08-08 02:05:51 +0800

    Updtae

--- pretest/findPost/index.js
+++ pretest/findPost/index.js
@@ -1,3 +1,14 @@
+let  scrollTo = (id) => {
+    if (id !== !1) {
+        document.getElementById(id)
+            .scrollIntoView({
+                block: "center"
+                , behavior: "smooth"
+            })
+        console.log("Hey")
+    }
+}
+
 let getId = (inTag) => {
     let tags = document.getElementsByTagName(inTag),
         counter = 0,
@@ -14,25 +25,31 @@ let getId = (inTag) => {
     return results
 }
 
-let checkText = (ids, text) => {
-
+let checkText =  (ids, text) => {
 
         let foundIDs = ids,
             post,
             i = 0,
             themeLength = foundIDs.length,
-            results = [],
-            counter = 0
-         for (; i < themeLength; i++) {
+            results = [!1, !1]
+
+    finder: for (; i < themeLength; i++) {
             post = document.getElementById(foundIDs[i])
 
             if (post.innerText.indexOf(text) !== -1) {
 
                 if(!post.querySelector('.message')) {
-                    console.log(post.innerText)
-                    return
+                     results = [post.querySelector('.postcontent.restore').parentNode.id, !0]
+                    console.log("1 - " + post.innerText)
+                    console.log("1 - " + results[0])
+                    document.getElementById(results[0]).style.color = 'red'
+                    break finder;
                 } else if (post.querySelector('.postcontent.restore').innerText.indexOf(text) !== -1){
-                    console.log(post.querySelector('.postcontent.restore').innerText)
+                    results = [post.querySelector('.postcontent.restore').parentNode.id, !0]
+                    console.log("2 - " + post.querySelector('.postcontent.restore').innerText)
+                    console.log("2 - " + results[0])
+                    document.getElementById(results[0]).style.color = 'red'
+                    break finder;
                 }
 
                 // if(post.querySelector('.message') !== null && post.querySelector('.message').innerText.indexOf(text) !== -1) {
@@ -41,12 +58,13 @@ let checkText = (ids, text) => {
 
             }
         }
-    // console.log(results)
+    return results
 }
 
 let allIds = getId('div')
-let searchStringPost = 'во Флориду, теперь вот'
+let searchStringPost = 'Здравствуйте! Мы сем'
 // let searchStringPost = 'В какой штат планируете?'
 let searchPost = checkText(allIds, searchStringPost)
+console.log("RESULTS: " + searchPost)
 
 
